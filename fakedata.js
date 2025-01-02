f8ffa45dc286210c4dbf85151cdb5d94814d04e0
siddhant <siddhant.saxena.2004@gmail.com>
2025-01-02 21:58:22 +0530

    fakedata

---
+++ fakedata.js
@@ -0,0 +1,37 @@
+const { faker } = require('@faker-js/faker');
+
+
+
+const path = require('path');
+
+const { v4: uuid } = require('uuid');
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'delta_app',
+    password: 'test123',
+    port: 3307
+});
+
+let createRandomUser = () => {
+    return [
+        faker.string.uuid(),
+        faker.internet.username(),
+        faker.internet.email(),
+        faker.internet.password()
+    ]
+};
+let users = [];
+for (let i = 0; i < 100; i++) {
+    users.push(createRandomUser());
+}
+let q = 'INSERT INTO user (id, username, email, password) VALUES ?';
+try {
+    connection.query(q, [users], (err, result) => {
+        if (err) throw err;
+        console.log(result);
+    });
+} catch (err) {
+    console.error(err);
+}
